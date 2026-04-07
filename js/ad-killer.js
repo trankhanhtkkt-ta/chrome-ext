728dc2298feb342a4afc27e32fb9ba3620efd4e8
Khanh Tran <khanh.tran@tecalliance.net>
2026-04-07 10:26:06 +0700

    Remove AdThrive ads

--- js/ad-killer.js
+++ js/ad-killer.js
@@ -1,15 +1,21 @@
-const removeAdFn = () => {
+const removeAdFn = (maxJitter) => {
+    const minJitter = 150;
     const adElements = [
         '[id="google_image_div"]'
         , '[id^="google_ads_iframe"]'
         , '[id^="gpt_unit_"]'
+        , '[id^="AdThrive_"]'
+        , '[class^="adthrive-"]'
     ];
     adElements.forEach(selector => {
         console.info("Removing ad element with selector: " + selector);
         $(selector).remove();
     });
+    setTimeout((maxJitter) => {
+        removeAdFn(maxJitter);
+    }, Math.max(minJitter, Math.random() * maxJitter));
 };
 
 $(() => {
-    setInterval(removeAdFn, 2000);
+    removeAdFn(2e3);
 });
